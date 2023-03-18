f95d6776699c9ed78af96ab67e540c063581eabf
eraqi <eraq@gmail.com>
2023-03-17 17:38:14 -0700

    last command

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if ((n%10) <6 && (n%10)!=0)
 	{
-		printf("Last digit of %d and is less than 6 and not 0\n",n , n % 10);
+		printf("Last digit of %d and is %d less than 6 and not 0\n",n , n % 10);
 	}
 	else 
 	{
