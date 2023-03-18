6d9a9a36a9024fabc1c69b748f37afc48bc2bd45
eraqi <eraq@gmail.com>
2023-03-17 18:00:41 -0700

    fix mistakes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - entry point
- *
- * Description: A C program that print with printf
- *
- * Return : Always 0 (success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
 
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- * @parameter: describe the parameter
- *
- * Return: Always 0 (Success)
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("Size of a char: %u byte(s)\n", sizeof(char));
-	printf("Size of a int: %u byte(s)\n", sizeof(int));
-	printf("Size of a long ini %u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int %u byte(s)\n", sizeof(long long int));
-	printf("Size of a float %u byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
+
